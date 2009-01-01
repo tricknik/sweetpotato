65d9dff843a778ca2275287f1b60d03e7a0bf0fc
Dmytri Kleiner <dk@telekommunisten.net>
2009-01-01 17:50:25 +0100

    xhtml extension

--- sweetpotato/extensions/xhtml.py
+++ sweetpotato/extensions/xhtml.py
@@ -72,8 +72,7 @@ class xhtml(htmlElement):
     >>> sp.addAdapter(xhtml)
     >>> sp.yaml(data)
     >>> sp.run('test')
-    >>> f = open('test.html').read()
-    >>> print f
+    >>> print open('test.html').read()
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html>
      <head>
